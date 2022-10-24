2237fe2df712db340176c27ee4e325f85ba48269
blackkong8 <blackkong8@gmail.com>
2022-10-24 23:28:53 +0900

    print mouse pos

--- mouse_test.c
+++ mouse_test.c
@@ -1,13 +1,66 @@
-//gcc mouse_test.c -framework ApplicationServices
+// For OSX
+// gcc mouse_test.c -framework ApplicationServices
 
 #include <ApplicationServices/ApplicationServices.h>
+#include <stdio.h>
 
-int main(void){
+void die(const char *s)
+{
+    perror(s);
+    exit(1);
+}
+
+void fancyPrintLocation(CGPoint location)
+{
+    printf(
+        "(%d, %d)\n",
+        (int)location.x,
+        (int)location.y);
+}
+
+CGEventRef CGEventCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon)
+{
+    if (type == kCGEventMouseMoved)
+    {
+        CGPoint location = CGEventGetLocation(event);
+        fancyPrintLocation(location);
+    }
+    return event;
+}
+
+int main(void)
+{
     CGRect screenBounds = CGDisplayBounds(CGMainDisplayID());
     printf(
         "Main screen is %dx%d\n",
         (int)screenBounds.size.width,
-        (int)screenBounds.size.height
-    );
+        (int)screenBounds.size.height);
+
+    CFMachPortRef eventTap = CGEventTapCreate(
+        kCGSessionEventTap,
+        kCGHeadInsertEventTap,
+        kCGEventTapOptionDefault,
+        (1 << kCGEventMouseMoved),
+        CGEventCallback,
+        NULL);
+
+    if (!eventTap)
+    {
+        die("eventTap");
+    }
+
+    CFRunLoopSourceRef runLoopSource = CFMachPortCreateRunLoopSource(
+        kCFAllocatorDefault,
+        eventTap,
+        0);
+
+    CFRunLoopAddSource(
+        CFRunLoopGetCurrent(),
+        runLoopSource,
+        kCFRunLoopCommonModes);
+
+    CGEventTapEnable(eventTap, true);
+    CFRunLoopRun();
+
     return 0;
 }
