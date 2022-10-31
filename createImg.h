abe1393661152181c7056ceac8e5cefb4012cc66
blackkong8 <blackkong8@gmail.com>
2022-10-31 14:38:11 +0900

    add createImg

---
+++ createImg.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+typedef struct Array2d
+{
+    int width, height, *data;
+} array2d;
+
+typedef struct Image
+{
+    char *image_name;
+    int scale;
+    array2d raw;
+} image;
+
+int writeImg(image img)
+{
+    FILE *pgmimg = fopen(img.image_name, "wb");
+    array2d raw = img.raw;
+    int width = raw.width, height = raw.height;
+
+    fprintf(pgmimg, "P2\n%d %d\n%d\n", width, height, img.scale);
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            fprintf(pgmimg, "%d ", *((raw.data + i * height) + j));
+        }
+        fprintf(pgmimg, "\n");
+    }
+
+    fclose(pgmimg);
+
+    return 0;
+}
